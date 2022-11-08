175969d1c5c4003bb0f5d7cb352de128c25fe80f
fulei <emil.fulei@gmail.com>
2022-11-08 15:58:15 +0100

    lol

--- 20/2advent.py
+++ 20/2advent.py
@@ -1,37 +1,43 @@
-# FILENAME = "input/test2.txt"
+#FILENAME = "input/test2.txt"
 FILENAME = "input/input2.txt"
 
-
 def main():
-    pass
+    total_list = load_data(FILENAME)
+    first_count = 0
+    second_count = 0
+    for line in total_list:
+        low, high, letter, string = line
+        count = 0
+        # First part
+        for s in string:
+            if s is letter:
+                count += 1
+                if count > high:
+                    break
+
+        if low <= count <= high:
+            first_count += 1
+        # Second
+        if (letter is string[low-1] and not letter is string[high-1]) or (letter is string[high-1] and not letter is string[low-1]):
+            second_count += 1
+    print(first_count, second_count)
+
 
 def load_data(path):
+    total_list = []
     with open(path) as f:
-        total_count = 0
-        liste = f.read().splitlines()
-        for line in liste:
-            split = line.split()
+        list = f.read().splitlines()
+        for line in list:
+            temp_list = [] # [Low ,High ,Letter, String]
+            split = line.split() # [Low-High, Letter:, String]
             numbers = split[0].split('-')
-
             
-            low_num = int(numbers[0])
-            high_num = int(numbers[1])
-            alhpa = split[1][0]
-
-
-            """ count = 0
-            for s in split[2]:
-                if s is alhpa:
-                    count += 1
-                    if count > high_num:
-                        break
-            if count < low_num:
-                continue
-            if low_num <= count <= high_num:
-                total_count += 1 """
-            if (split[2][low_num-1] is alhpa and not split[2][high_num-1] is alhpa ) or split[2][high_num-1] is alhpa and not split[2][low_num-1] is alhpa:
-                total_count += 1
-        print(total_count)
+            temp_list.append(int(numbers[0])) # Low
+            temp_list.append(int(numbers[1])) # High
+            temp_list.append(split[1][0]) # Letter
+            temp_list.append(split[2]) # string
+            total_list.append(temp_list)
+    return total_list
 
 if __name__ == '__main__':
-    load_data(FILENAME)
+    main()
