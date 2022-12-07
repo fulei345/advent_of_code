2262948eb3dd6a101d36b3629fdc02d8b7bb977a
Fulei <emil.fulei@gmail.com>
2022-12-07 21:36:03 +0100

    optimise

--- 22/7advent.py
+++ 22/7advent.py
@@ -1,73 +1,80 @@
 def main(filename):
     cd = Directory("", None)
+    all_folders =[]
     with open(filename) as f:
         for line in f:
             cmdline = line[0:len(line)-1]
             cmdline = cmdline.split(" ")
+            # Command
             if cmdline[0] == "$":
                 if cmdline[1] == "cd":
                     if cmdline[2] == "/":
                         cd = Directory("/", None)
                     elif cmdline[2] == "..":
+                        # Count folder and go up
                         if cd.parent != None:
+                            all_folders.append(count_folder(cd))
                             cd = cd.parent
-                            # Count dirs and send that also
                     else:
+                        # Change directory
                         cd = cd.dirs[cmdline[2]]
                 elif cmdline[1] == "ls":
                     pass
             else:
+                # List dir
                 if cmdline[0] == "dir":
                     temp = Directory(cmdline[1], cd)
                     cd.dirs[cmdline[1]] = temp
                 else:
+                    # List file
                     cd.files[cmdline[1]] = int(cmdline[0])
-
+        
+    # Count the rest of the folders going up
     while cd.parent != None:
+        all_folders.append(count_folder(cd))
         cd = cd.parent
+    all_folders.append(count_folder(cd))
+    all_folders.sort()
 
-    all_sizes = []
-    count_files(cd,all_sizes)
-    all_sizes.sort()
-
+    # Calc space needed to delete
     total_size = 70000000
     upgrade = 30000000
-    used = all_sizes[-1]
+    used = all_folders[-1]
     unused = total_size - used
     free_space = upgrade - unused
     
+    # Find folder to delete
     index = -1
     temp_total = 70000000
     while temp_total > free_space:
         index -= 1
-        temp_total = all_sizes[index]
-    print(all_sizes[index+1])
+        temp_total = all_folders[index]
+    print(all_folders[index+1])
 
-
-    while cd.parent != None:
-        cd = cd.parent
     print(part1(cd))
 
-    
-def count_files(cd, list):
-    for key, value in cd.files.items():
-        cd.fullsize += value
-    for key, value in cd.dirs.items():
-        cd.fullsize += count_files(cd.dirs[key],list)
-    if cd.fullsize <= 100000:
-        cd.under = True
-    list.append(cd.fullsize)
-    return cd.fullsize
-
+# Find size of small folders
 def part1(cd):
     temp = 0
     for key, value in cd.dirs.items():
         temp += part1(cd.dirs[key])
-    if cd.under:
+    if 100000 >= cd.fullsize:
         temp += cd.fullsize
     return temp
 
+# Count files in folder and add to parent
+def count_folder(cd):
+    for key, value in cd.files.items():
+        cd.fullsize += value
+    if cd.parent != None:
+        cd.parent.fullsize += cd.fullsize
+    return cd.fullsize
+
 class Directory:
+    # Name of the folder
+    # Dictionary of its files
+    # Pointer to parent dict
+    # Total size
     def __init__(self, name, parent):
         self.name = name
         self.files = dict()
@@ -77,18 +84,18 @@ class Directory:
         self.fullsize = 0
         
     def __str__(self) -> str:
-        lol = "Name: "+ self.name + "\n"
-        lol += "Files: "
+        result = "Name: "+ self.name + "\n"
+        result += "Files: "
         for key, value in self.files.items():
-            lol += key + " "
-        lol += "Dirs: "
+            result += key + " "
+        result += "Dirs: "
         for key, value in self.dirs.items():
-            lol += key + " "
-        lol += "\nFullsize: " + str(self.fullsize) 
-        return lol
+            result += key + " "
+        result += "\nFullsize: " + str(self.fullsize) 
+        return result
 
 
 
 if __name__ == "__main__":
-    # main("input/test7.txt")
+    main("input/test7.txt")
     main("input/input7.txt")
