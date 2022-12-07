c52df208b8ce194c842472976dc71cfd0a4aa217
Fulei <emil.fulei@gmail.com>
2022-12-07 09:08:11 +0100

    6 done

--- 22/6advent.py
+++ 22/6advent.py
@@ -1,15 +1,21 @@
-def check_not_common(liste):
+def check_not_common(liste, length):
     set_var = set(liste)
-    return len(set_var) == 4
+    return len(set_var) == length
 
 def main(filename):
     with open(filename) as f:
         for line in f:
             communication = line[0:len(line)-1]
             for i in range(len(communication) - 3):
-                if check_not_common(communication[i:i+4]):
+                if check_not_common(communication[i:i+4],4):
                     print(i+4)
                     break
+            for i in range(len(communication) - 13):
+                if check_not_common(communication[i:i+14],14):
+                    print(i+14)
+                    break
+
+            
 
 
 
