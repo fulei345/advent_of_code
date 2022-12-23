853b203f1c54e2a1b6a34934715afca4b7a294b3
fulei <emil.fulei@gmail.com>
2022-12-23 09:46:37 +0100

    lol

--- 22/13advent.py
+++ 22/13advent.py
@@ -5,6 +5,7 @@ def main(filename):
         left_done = False
         right_done = False
         index = 1
+        result = []
         for line in f:
             if line == "\n":
                 left = []
@@ -31,17 +32,70 @@ def main(filename):
                     left,_ = parse_list(l,0)
 
             if left_done and right_done:
-                print(left)
-                print(right)
+                # print(left)
+                # print(right)
+                temp = compare_lists(left, right)
+                if temp:
+                    result.append(index)
+                # print(compare_lists(left, right))
+                index += 1
+    print(sum(result))
 
 def compare_lists(left: list, right: list):
-    if len(left) == 0:
+    l_len = len(left)
+    r_len = len(right)
+    
+    if l_len == 0 and r_len == 0:
+        return None
+    elif l_len == 0:
         return True
-    elif len(right) == 0:
+    elif r_len == 0:
         return False
+    index = 0
+    max_length = min(l_len, r_len)
+    while index < max_length:
+        l = left[index]
+        r = right[index]
+        l_type = type(l)
+        r_type = type(r) 
+        if l_type == r_type:
+            if l_type == int:
+                result = compare_num(l, r)
+                if result is not None:
+                    return result
+                elif not result:
+                    return False
+            else:
+                result = compare_lists(l, r)
+                if result is not None:
+                    return result
+        else:
+            if l_type == int:
+                l = [l]
+            else:
+                r = [r]
+            result = compare_lists(l, r)
+            if result is not None:
+                return result
+        index += 1
+    if l_len == index and r_len == index:
+        return None
+    elif l_len == index:
+        return True
+    elif r_len == index:
+        return False
+
+
+                            
     # Something about indexing ?
 
-
+def compare_num(left: int, right: int):
+    if left < right:
+        return True
+    elif right < left:
+        return False
+    else:
+        return None
 
 
 def parse_list(l: list, index):
@@ -65,5 +119,5 @@ def parse_list(l: list, index):
     return None
 
 if __name__ == "__main__":
-    main("22/input/test13.txt")
-    # main("22/input/input13.txt")
+    # main("22/input/test13.txt")
+    main("22/input/input13.txt")
